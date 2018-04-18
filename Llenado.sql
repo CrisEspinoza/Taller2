a85989a534440e45d1bb571a2d4e4efa4fd97a2c
Huaso <cristian.espinoza.s@usach.cl>
2018-04-18 15:17:04 -0300

    Subiendo el metodo post de actor y un llenado para la base de datos

---
+++ Llenado.sql
@@ -0,0 +1,16 @@
+use sakila;
+
+INSERT INTO `actor` (`actor_id`, `first_name`, `last_name`, `last_update`) VALUES (1, 'CRISTIAN', 'ESPINOZA', '2018-04-18 10:59:43');
+INSERT INTO `actor` (`actor_id`, `first_name`, `last_name`, `last_update`) VALUES (2, 'CRISTOFER', 'ESPINOZA', '2018-04-18 10:59:43');
+INSERT INTO `actor` (`actor_id`, `first_name`, `last_name`, `last_update`) VALUES (3, 'CAROLINA', 'ESPINOZA', '2018-04-18 10:59:43');
+INSERT INTO `actor` (`actor_id`, `first_name`, `last_name`, `last_update`) VALUES (4, 'PRINCESA', 'ESPINOZA', '2018-04-18 10:59:43');
+INSERT INTO `actor` (`actor_id`, `first_name`, `last_name`, `last_update`) VALUES (5, 'KEYLA', 'CHANDIA', '2018-04-18 10:59:43');
+
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (1, 'RAPIDO Y FURIOSO 1');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (2, 'RAPIDO Y FURIOSO 2');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (3, 'RAPIDO Y FURIOSO 3');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (4, 'RAPIDO Y FURIOSO 4');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (5, 'RAPIDO Y FURIOSO 5');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (6, 'RAPIDO Y FURIOSO 6');
+INSERT INTO `film` (`films_id`, `first_name`) VALUES (7, 'RAPIDO Y FURIOSO 7');
+
--- Taller2-TBD/src/main/java/Taller1db/TBD/rest/ActorService.java
+++ Taller2-TBD/src/main/java/Taller1db/TBD/rest/ActorService.java
@@ -5,20 +5,17 @@ import Taller1db.TBD.repository.ActorRepository;
 import Taller1db.TBD.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import Taller1db.TBD.Entities.Film;
+
 import java.util.Set;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import java.sql.Timestamp;
-
-
 import java.util.List;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/actors")
 public class ActorService {
@@ -29,14 +26,12 @@ public class ActorService {
     private FilmRepository filmRepository;
 
 
-    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public List<Actor> getAllActors() {
         return actorRepository.findAll();
     }
 
-    @CrossOrigin
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Actor getActor(@PathVariable Integer id) {
@@ -44,7 +39,6 @@ public class ActorService {
         return actorRepository.findActorById(actorId);
     }
 
-    @CrossOrigin
     @RequestMapping(value = "/{id}/films", method = RequestMethod.GET)
     @ResponseBody
     public Set<Film> movieTheActor (@PathVariable("id") Long id) {
@@ -53,27 +47,19 @@ public class ActorService {
 
     @PostMapping("/{id}/films/{idFilms}")
     @ResponseBody
-    public ResponseEntity<Set<Film>> match (@PathVariable("id") Long id, @PathVariable("idFilms") Long idFilms) {
+    public HttpStatus match (@PathVariable("id") Long id, @PathVariable("idFilms") Long idFilms) {
 
         Actor actor = actorRepository.findActorById(id);
         Film film = filmRepository.findFilmsById(idFilms);
-
-        if (film == null || actor == null ){
-
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        else{
-            Set<Film> films = actor.getFilms();
-            films.add(film);
-            actor.setFilms(films);
+        if ( actor !=  null &&  film != null) {
+            actor.getFilms().add(film);
             actorRepository.save(actor);
-            return new ResponseEntity<Set<Film>>(actorRepository.findActorById(id).getFilms(), HttpStatus.OK);
-
+            return HttpStatus.OK;
         }
+        else
+            return HttpStatus.NOT_ACCEPTABLE;
     }
 
-    @CrossOrigin
     @PostMapping("/create")
     @ResponseBody
     public Actor create(@RequestBody Actor resource) throws ParseException {
