d5c1c0da633f7768d673ca6fca1b529353563eb1
Huaso <cristian.espinoza.s@usach.cl>
2018-04-18 11:01:23 -0300

    Agregando nuevos metodos de rest

--- Taller2-TBD/src/main/java/Taller1db/TBD/rest/ActorService.java
+++ Taller2-TBD/src/main/java/Taller1db/TBD/rest/ActorService.java
@@ -2,7 +2,10 @@ package Taller1db.TBD.rest;
 
 import Taller1db.TBD.Entities.Actor;
 import Taller1db.TBD.repository.ActorRepository;
+import Taller1db.TBD.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import Taller1db.TBD.Entities.Film;
 import java.util.Set;
@@ -22,6 +25,9 @@ public class ActorService {
 
     @Autowired
     private ActorRepository actorRepository;
+    @Autowired
+    private FilmRepository filmRepository;
+
 
     @CrossOrigin
     @RequestMapping(method = RequestMethod.GET)
@@ -43,6 +49,28 @@ public class ActorService {
     @ResponseBody
     public Set<Film> movieTheActor (@PathVariable("id") Long id) {
         return actorRepository.findActorById(id).getFilms();
+    }
+
+    @PostMapping("/{id}/films/{idFilms}")
+    @ResponseBody
+    public ResponseEntity<Set<Film>> match (@PathVariable("id") Long id, @PathVariable("idFilms") Long idFilms) {
+
+        Actor actor = actorRepository.findActorById(id);
+        Film film = filmRepository.findFilmsById(idFilms);
+
+        if (film == null || actor == null ){
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        else{
+            Set<Film> films = actor.getFilms();
+            films.add(film);
+            actor.setFilms(films);
+            actorRepository.save(actor);
+            return new ResponseEntity<Set<Film>>(actorRepository.findActorById(id).getFilms(), HttpStatus.OK);
+
+        }
     }
 
     @CrossOrigin
