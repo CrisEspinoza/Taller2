8d05fb7061744e1b8aec124d3beaa68bb5fe248a
Verónica <veronica.calas@usach.cl>
2018-04-21 22:44:42 -0300

    Obtenciones peliculas/actores listas

--- Taller2-Front/plantilla-vue/src/viewMovies.vue
+++ Taller2-Front/plantilla-vue/src/viewMovies.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="center">
         <h2>{{title}}</h2>
-        <ul class = "movies-list">
+        <ul class = "user-list">
             <li v-for="u,i in movies">
                     <img :src="'https://robohash.org/'+i+'?size=50x50'" />
-                    <span>{{u.firstName}}</span>
+                    <span>{{u.id}}.- {{u.firstName}}</span>
+<hr>
+            <button class="button_miau" @click=findActors(u.id)>+</button>
             </li>
+            <ul class="user-list">
+                <li v-for="actor in actors">
+                    <div class="isa_info"> <i class="fa fa-info-circle"></i>{{actor.firstName}} {{actor.lastName}}
+                    </div>
+
+                </li>
+            </ul>
+
         </ul>
     </div>
+
 
 </template>
 
@@ -19,6 +30,7 @@ export default {
         return{
           title:'Lista peliculas',
           movies: [],
+            actors: [],
           submitted: false,
           error:false
         }
@@ -34,7 +46,22 @@ export default {
                // error callback
                console.log('Error cargando lista de peliculas');
             })
-      }
+      },
+
+    methods:{
+        findActors(value){
+            console.log(value);
+            this.$http.get('http://localhost:8081/films/'+value+'/actors')
+                .then(response=>{
+                    //get films from actor
+                    this.actors = response.body;
+
+
+                },response => {
+                    console.log('error leyendo peliculas');
+                });
+        }
+    }
 }
 
 </script>
